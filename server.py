d1719fb110824f3a95772129516f103d45e3cbd4
Josh <joshua_lu1@brown.edu>
2025-01-03 23:59:34 -0500

    introduce threading to handle multiple clients at once

--- server.py
+++ server.py
@@ -1,4 +1,29 @@
 import socket
+import threading
+
+
+
+# function to handle communication with a single client
+
+def handle_client(client_socket, client_address):
+    print(f"Connection established with {client_address}")
+
+    while True:
+        # receive data from the client
+        data = client_socket.recv(1024).decode()
+        # if no data, close.
+        if not data:
+            print(f"Connection with {client_address} closed.")
+            break
+        print(f"Client {client_address} says: {data}")
+
+        # Send a response to the client
+        message = f"Received your message"
+        client_socket.send(message.encode())
+    # close the client connection
+    client_socket.close()
+
+
 
 # create a socket
 # specifies the address, AF_INET
@@ -21,22 +46,12 @@ s.listen(5)
 print("Server set up. Now listening for requests...")
 
 while(True):
-    #accept a new client connection (and waits for next connection)
-    #c is new socket object for communication
-    #a is address of the connected client (IP and port)
-    c, a = s.accept() 
+    # accept a new client connection (and waits for next connection)
+    # c is new socket object for communication
+    # a is address of the connected client (IP and port)
+    client_socket, client_address = s.accept() 
 
-    while True:
-        # Receive data from the client
-        data = c.recv(1024).decode()
-        if not data:  # If no data, client has closed the connection
-            print(f"Connection with {a} closed.")
-            break
-        print(f"Client {a} says: {data}")
-
-        # Send a response to the client
-        message = f"Received your message"
-        c.send(message.encode())
-
-    # Close client connection
-    c.close()
+    # create a new thread that will ahdnle the client
+    # this allows server to handle multiple clients at once
+    client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
+    client_thread.start()
